dc855d4e9ff4f665cea4925806da75c17c610868
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-05 11:06:52 +0100

    react icons

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,13 +1,13 @@
 import css from './Contact.module.css'
-import { useId } from 'react'
+import { FaUser, FaPhone } from "react-icons/fa";
 
 const Contact = ({ data: { id, name, number}, onDelete }) => {
 
   return (
     <div className={css.contactContainer}>
       <ul className={css.contactInfo}>
-        <li className={css.contactName}>{name}</li>
-        <li className={css.contactNumber}>{number}</li>
+        <li className={css.contactName}><FaUser />{name}</li>
+        <li className={css.contactNumber}><FaPhone />{number}</li>
       </ul>
       <button className={css.contactBtn} onClick={() => onDelete(id)}>Delete</button>
     </div>
