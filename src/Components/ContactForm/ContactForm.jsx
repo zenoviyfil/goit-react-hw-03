c1f10adb045212430712a5c8996e7039df994685
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-06 12:46:46 +0100

    experiment

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -2,6 +2,7 @@ import css from './ContactForm.module.css'
 import { Formik, Form, ErrorMessage } from 'formik';
 import { useId } from 'react';
 import { date } from 'yup';
+import { FaUser, FaPhone, FaPlus } from "react-icons/fa";
 
 const ContactForm = ({ onAdd }) => {
   const handleSubmit = e => {
@@ -16,9 +17,11 @@ const ContactForm = ({ onAdd }) => {
 
   return(
     <form className={css.form} onSubmit={handleSubmit}>
-      <input className={css.formName} type="text" name='text'/>
-      <input className={css.formNumb} type="number" name='number'/>
-      <button className={css.formBtn} type='submit'>Add contact</button>
+      <label className={css.inputLabelUser} htmlFor="text"><FaUser /></label>
+      <input className={css.formName} type="text" name='text' placeholder='George Washington'/>
+      <label className={css.inputLabelNumber} htmlFor="number"><FaPhone /></label>
+      <input className={css.formNumb} type='tel' name='number' placeholder='0667898456'/>
+      <button className={css.formBtn} type='submit'><FaPlus />Add contact</button>
     </form>
   )
 };
