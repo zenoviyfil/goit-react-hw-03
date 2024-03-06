3bea9099af1ce50a7330630e0a7fe4748be9b4b8
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-06 12:01:48 +0100

    localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as Yup from 'yup';
 import ContactForm from './Components/ContactForm/ContactForm'
 import SearchBox from './Components/SearchBox/SearchBox'
 import ContactList from './Components/ContactList/ContactList'
 
+const contactsData = [
+  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+];
+
 function App() {
   // Contact Form
-  const [contacts, setContacts] = useState([
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ]
-  )
+  const [contacts, setContacts] = useState(() => {
+    const stringMails = localStorage.getItem('contacts')
+    if (!stringMails) return contactsData;
+
+    const parsedMails = JSON.parse(stringMails);
+    return parsedMails;
+  })
+
+  useEffect (() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts])
 
   const addContact = (newContact) => {
     setContacts((initContacts) => {
